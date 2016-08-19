238a2b7051221bf82b4226c85fb066ebe7fc1b2f
Gregory Daynes <gregdaynes@gmail.com>
2016-08-19 16:13:07 -0700

    stuffs

--- app/services/github.js
+++ app/services/github.js
@@ -1,59 +1,11 @@
-export default function henryAuthGithubService($rootScope, locker, GITHUB_UPDATE_MESSAGE) {
-    const sessionStorage = locker.namespace('henry')
-                                 .driver('session');
+import Github from "github-api";
 
-    let gh = null;
+let github = null;
 
-    return {
-        set,
-        get,
-        onUpdate,
-        unset,
+export default function() {
+    return (username, password) => {
+        if (!github) github = new Github({ username, password });
+
+        return github;
     };
-
-    // -------------------------
-
-    function _broadcast(data) {
-        $rootScope.$broadcast(GITHUB_UPDATE_MESSAGE, {
-            data,
-        });
-    }
-
-    // -------------------------
-
-    function set(data) {
-        gh = data;
-        sessionStorage.put('gh', gh);
-
-        _broadcast(gh);
-
-        return Promise.resolve().then(() => data);
-    }
-
-    function get() {
-        const promise = Promise.resolve();
-
-        if (!gh) { // no cached auth
-            gh = sessionStorage.get('gh');
-        }
-
-        _broadcast(gh);
-
-        return promise.then(() => gh);
-    }
-
-    function onUpdate($scope, handler) {
-        $scope.$on(GITHUB_UPDATE_MESSAGE, (event, message) => {
-            handler(message.data);
-        });
-    }
-
-    function unset() {
-        gh = null;
-        sessionStorage.empty();
-
-        _broadcast(null);
-
-        return Promise.resolve().then(() => gh);
-    }
 }
