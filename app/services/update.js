3581164b38a389897a9921ae74db9b0369c6757f
Gregory Daynes <gregdaynes@gmail.com>
2016-09-04 20:46:41 -0700

    commit to demo + dev capabilities of auto updating

--- app/services/update.js
+++ app/services/update.js
@@ -1,4 +1,4 @@
-export default function updateService(githubService) {
+export default function updateService($log, githubService) {
     'ngInject';
 
     let gh = null;
@@ -7,7 +7,7 @@ export default function updateService(githubService) {
     const service = {
         init,
         checkUpdate,
-        getUpdate,
+        update,
     };
 
     return service;
@@ -23,24 +23,34 @@ export default function updateService(githubService) {
     }
 
     function checkUpdate() {
-        return getManifest();
-        // connect to github,
-        // get contents of repo update manifest
-        // compare versions
+        return getManifest()
+            .then(manifest => compareVersion(config.version, manifest.data.version))
+            .then(newVersion => {
+                if (newVersion) return newVersion;
 
-        // return version to update
-        // // error
+                throw new Error();
+            })
+            .catch(() => false);
     }
 
-    function getUpdate() {
-        // connect to github
-        // get contents of repo update manifest
-        // compare versions again - no double updating
-        //
+    function update() {
+        return getManifest()
+            .then(manifest => compareVersion(config.version, manifest.data.version))
+            .then(newVersion => {
+                if (newVersion) return newVersion;
+
+                throw new Error();
+            })
+            .then(getUpdate)
+
+            .catch(err => {
+                $log.error(err);
+                throw new Error(err);
+            });
+
         // get contents of update
         // write contents to local henry
         // return update complete
-        // // error
     }
 
     // --------------------------------
@@ -51,7 +61,23 @@ export default function updateService(githubService) {
     }
 
     function getManifest() {
-        return gh.getContents(config.update.branch, config.update.versionFile)
-            .then(console.log);
+        return gh.getContents(config.update.branch, config.update.versionFile, true);
+    }
+
+    function compareVersion(first, second) {
+        const promise = Promise.resolve();
+
+        const fv = first.split('.');
+        const sv = second.split('.');
+
+        if (fv[0] < sv[0]) return promise.then(() => true); // major
+        if (fv[0] === sv[0] && fv[1] < sv[1]) return promise.then(() => true); // minor
+        if (fv[0] === sv[0] && fv[1] === sv[1] && fv[2] < sv[2]) return promise.then(() => true); // patch
+
+        return promise.then(() => second);
+    }
+
+    function getUpdate() {
+        
     }
 }
